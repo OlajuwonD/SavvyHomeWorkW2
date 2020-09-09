c131a1ae6d769575e57d21d9a62e8f14592ba244
Olajuwon Davis <olajuwonadavis@gmail.com>
2020-09-09 18:22:14 -0500

    Week2HW

---
+++ Week2HMWK.js
@@ -0,0 +1,16 @@
+let pizzaPlace = "Slice of Heaven";
+let numberOfToppings = 25;
+
+console.log(typeof 25);
+console.log(typeof "Slice of Heaven");
+
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+
+console.log(`Hello, Welcome to ${pizzaPlace}, where we have up to ${numberOfToppings} delicious toppings!`)
+
+if (numberOfToppings <= 10) {
+  console.log("Quality, not Quantity");
+} if (numberOfToppings > 10) {
+  console.log(`"A Whole lot of Pizza"`);
+};
