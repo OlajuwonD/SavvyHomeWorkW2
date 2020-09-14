d6d8a0fe2aa441627cd50a54fc71bbd54e9cffd5
Olajuwon Davis <olajuwonadavis@gmail.com>
2020-09-14 16:58:10 -0500

    tryning hmwk3

--- wk3hmk.js
+++ wk3hmk.js
@@ -3,12 +3,23 @@ const pizzaToppings = [
   "cheese",
   "peppers",
   "garlic",
+
 ];
 
-for (let toppings of pizzaToppings) {
-  console.log(toppings);
-}
-let greetCustomer = `"Welcome to Pizze House, our toppings are: ${toppings}`;
 
-console.log(greetCustomer);
+// for (let toppings of pizzaToppings) {
+//     //  console.log(toppings);
+// };
+
+let greetCustomer = `"Welcome to Pizza House, our toppings are: ${pizzaToppings}"`
+// console.log(greetCustomer);
+
+const getPizzaOrder = {
+  size: ["small", "medium", "large"],
+  crust: ["thick", "thin"],
+  toppings: [pizzaToppings],
+
+};
+
+console.log(getPizzaOrder);
 
