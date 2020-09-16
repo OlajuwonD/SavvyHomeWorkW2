362c769e4f6a7a943f9b5fffa0097334ded79e75
Olajuwon Davis <olajuwonadavis@gmail.com>
2020-09-16 18:27:36 -0500

    HWK3

--- wk3hmk.js
+++ wk3hmk.js
@@ -11,15 +11,27 @@ const pizzaToppings = [
 //     //  console.log(toppings);
 // };
 
-let greetCustomer = `"Welcome to Pizza House, our toppings are: ${pizzaToppings}"`
+let greetCustomer = `Welcome to Pizza House, our toppings are: ${pizzaToppings}`
 // console.log(greetCustomer);
 
 const getPizzaOrder = {
   size: ["small", "medium", "large"],
   crust: ["thick", "thin"],
-  toppings: [pizzaToppings],
+  toppings: [...pizzaToppings]
 
-};
+}
 
-console.log(getPizzaOrder);
+  console.log(`One ${getPizzaOrder.size[0]} ${getPizzaOrder.crust[1]} crust pizza with ${getPizzaOrder.toppings} coming UP!`);
 
+
+const preparePizza = {
+  size: getPizzaOrder.size,
+  crust: getPizzaOrder.crust,
+  toppings: getPizzaOrder.toppings
+}
+
+console.log(`Cooking Pizza`, preparePizza)
+
+const servePizza = preparePizza(size, crust, toppings);
+
+console.log()
